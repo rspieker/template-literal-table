ec01f1c61e944c283992f58b90f682538ca27f04
Rogier Spieker <rogier@konfirm.net>
2020-12-19 08:47:14 +0100

    compacting modified the internal array

--- source/Cell.ts
+++ source/Cell.ts
@@ -15,46 +15,24 @@ const space = Character.from(' ');
  */
 export class Cell extends Collection<Value> {
 	/**
-	 * trim
-	 * Removes all edge (leading/trailing) spacing from the cell
-	 *
-	 * @returns {Cell}
-	 * @memberof Cell
-	 */
-	trim(): Cell {
-		const { items } = this;
-		let first = 0;
-
-		//  determine the last sequential space token from the start
-		while (items[first] === space) {
-			++first;
-		}
-		//  remove the leading space tokens
-		items.splice(0, first);
-
-		//  while the last Character is a space token, reduce the items by one
-		while (items[items.length - 1] === space) {
-			items.length -= 1;
-		}
-
-		return this;
-	}
-
-	/**
 	 * compact
-	 * Rebuild the cell contents into the desired shape, either consisting of
-	 * a single type or a string (if the trimmed input consist of multiple concetanated types)
+	 * Rebuild the cell contents into the cleaned up result, either consisting
+	 * of a single type (as provided) or a string (if the trimmed input consist
+	 * of multiple concetanated types)
 	 *
 	 * @returns
 	 * @memberof Cell
 	 */
 	compact() {
-		const { items } = this.trim();
+		const { items: work } = this;
+		let before = 0;
+		let after = work.length;
 
-		if (items.length > 1) {
-			return items.map((token) => token.value).join('');
-		}
+		while (work[before] === space) ++before;
+		while (work[after - 1] === space) --after;
 
-		return items.length ? items[0].value : undefined;
+		const slice = work.slice(before, after).map(({ value }) => value)
+
+		return slice.length > 1 ? slice.join('') : slice[0];
 	}
 }
